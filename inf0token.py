9a644f8dd9c5c4ab4e59bcd6f5f3342cdbec8834
valentine <v@taehyung.org>
2021-07-08 18:35:18 -0400

    Update inf0token.py

--- inf0token.py
+++ inf0token.py
@@ -1,74 +1,45 @@
 #!/usr/bin/python3
-import pkg_resources, os, sys, subprocess
-from sys import platform
-
-# Checking if there are missing packages.
-required = {'requests', 'argparse'}
-installed = {pkg.key for pkg in pkg_resources.working_set}
-missing = required-installed 
-
-# Ensure the valid OS.
-def check_os(python):
-	if platform == "linux":
-		os.system("pip3 install requests argparse --user")
-	elif platform == "win32":
-		subprocess.check_call([python, '-m', 'pip', 'install', *missing])
-	return "[+] Installed all packages"
-
-# Installs missing packages.
-def install_missing():
-	if missing:
-		python = sys.executable
-		check_os(python)
-	else:
-		pass
-
-# Import the packages that were installed or already installed
-import requests, json, argparse, time
-
-# Parsing arguments.
-parser = argparse.ArgumentParser()
-
-# Adding arguments.
-parser.add_argument("t", help="Discord token of the target", metavar="TOKEN")
-
-# Read arguments from command line.
-args = parser.parse_args()
+import requests, json, sys
+from time import sleep
 
 # Base variables
-url = "https://discord.com/api/v6/users/@me"
-h = {"Authorization": args.t} # headers
-r = requests.get(url, headers=h) # GET request to Discord API
+url = "https://discord.com/api/v9/users/@me"
 
 # Check the token
-def check_token():
+def check_token(token):
+	headers = {"Authorization": token}
+	r = requests.get(url, headers=headers) # GET request to Discord API
 	if r.status_code == 200:
 		print("\n[+] Token passed verification check... gathering info")
-		time.sleep(2)
+		sleep(2)
 	else:
-		print("[!] Token invalid " + f"({r.status_code} Error)")
+		print(f"[!] Token invalid, Error: {r.status_code}")
 		sys.exit()
 
 # Gather info
-def gather_info():
+def gather_info(token):
+	check_token(token)
+	sleep(2)
+	headers = {"Authorization": token}
+	r = requests.get(url, headers=headers) # GET request to Discord API
 	data = r.json()
-	print(f"[+] Account: {data['username']}#{data['discriminator']} - {data['id']}")
-	if data['verified'] == True:
-		print(f"[+] Email: {data['email']} - Verified")
-	else:
-		print(f"[+] Email: {data['email']} - Not verified")
-	print(f"[+] Phone Number: {data['phone']}")
-	if data['mfa_enabled'] == True:
-		print(f"[+] GAuth is enabled")
-	else:
-		print(f"[+] GAuth is disabled, have at it! :D")
+	verified = f"{data['email']} - Verified" if data["verified"] is True else f"{data['email']} - Not Verified"
+	gauth_status = "Enabled" if data["mfa_enabled"] is True else "Disabled"
+
+	print(f'''
+[+] Account: {data['username']}#{data['discriminator']} - {data['id']}
+[+] Phone Number: {data['phone']}
+[+] Email: {verified}
+[+] GAuth: {gauth_status}
+		''')
 
 def main():
-	install_missing()
-	time.sleep(2)
-	check_token()
-	gather_info()
-	print("\n~ valentine\n")
+	if len(sys.argv) < 2:
+		print(f"{sys.argv[0]} TOKEN")
+	else:
+		token = sys.argv[1]
+		gather_info(token)
+		print("~ valentine\n")
 
 if __name__ == "__main__":
 	main()
